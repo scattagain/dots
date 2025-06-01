c827bc130c42a78c21fefb8e0b7c81849b74223e
scattagain <61865847+scattagain@users.noreply.github.com>
2025-06-01 20:58:43 +0500

    Fix constraint for middle widget

--- awesome/wibar/middle.lua
+++ awesome/wibar/middle.lua
@@ -6,25 +6,29 @@ return function(screen)
     return {
         widget = wibox.container.constraint,
         width = 900,
-        layout = wibox.layout.fixed.horizontal,
 
         {
-            left = beautiful.wibar_widget_gap * 4,
-            right = beautiful.wibar_widget_gap * 4,
-            draw_empty = false,
-            widget = wibox.container.margin,
 
-            tasklist.focused(screen),
+            layout = wibox.layout.fixed.horizontal,
 
-        },
+            {
+                left = beautiful.wibar_widget_gap * 4,
+                right = beautiful.wibar_widget_gap * 4,
+                draw_empty = false,
+                widget = wibox.container.margin,
 
-        {
-            left = beautiful.wibar_widget_gap * 4,
-            right = beautiful.wibar_widget_gap * 4,
-            draw_empty = false,
-            widget = wibox.container.margin,
+                tasklist.focused(screen),
 
-            tasklist.minimized(screen),
+            },
+
+            {
+                left = beautiful.wibar_widget_gap * 4,
+                right = beautiful.wibar_widget_gap * 4,
+                draw_empty = false,
+                widget = wibox.container.margin,
+
+                tasklist.minimized(screen),
+            }
         }
     }
 end

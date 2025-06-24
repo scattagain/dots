c3ea4099c52eb58fa68c101ce50b88a862a2125a
scattagain <61865847+scattagain@users.noreply.github.com>
2025-06-24 13:32:09 +0500

    Fixed some issues with the spacing

--- awesome/wibar/middle.lua
+++ awesome/wibar/middle.lua
@@ -1,34 +1,20 @@
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local tasklist  = require("widgets.tasklist")
+local spacing = require("widgets.spacing")
 
 return function(screen)
     return {
         widget = wibox.container.constraint,
+        layout = wibox.layout.fixed.horizontal,
+        spacing = beautiful.wibar_widget_gap,
         width = 900,
 
-        {
+        spacing.padding,
 
-            layout = wibox.layout.fixed.horizontal,
+        tasklist.focused(screen),
+        tasklist.unfocused(screen),
 
-            {
-                left = beautiful.wibar_widget_gap * 4,
-                right = beautiful.wibar_widget_gap * 2,
-                draw_empty = false,
-                widget = wibox.container.margin,
-
-                tasklist.focused(screen),
-
-            },
-
-            {
-                left = beautiful.wibar_widget_gap * 2,
-                right = beautiful.wibar_widget_gap * 4,
-                draw_empty = false,
-                widget = wibox.container.margin,
-
-                tasklist.unfocused(screen),
-            }
-        }
+        spacing.padding
     }
 end

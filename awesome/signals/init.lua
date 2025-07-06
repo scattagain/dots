447ef1f63c133d46658f33e722f1beb7155fbcba
scattagain <61865847+scattagain@users.noreply.github.com>
2025-07-06 07:55:59 +0500

    Did something with the signals, i forgor 💀

--- awesome/signals/init.lua
+++ awesome/signals/init.lua
@@ -1,6 +1,5 @@
 return {
     naughty = require("signals.naughty"),
-    client = require("signals.client"),
     screen = require("signals.screen"),
-    awesome = require("signals.awesome")
+    client = require("signals.client"),
 }
